7c405080c839ec2438307e2efe447a8c32d1a33d
Tiago Marinho <tiagomeraki@gmail.com>
2025-10-28 15:24:42 +0100

    remove leftover debug log

--- backend/src/controllers/chatController.js
+++ backend/src/controllers/chatController.js
@@ -4,7 +4,6 @@ import Message from '../models/Message.js';
 export const sendMessage = async (req, res) => {
 	const { text, params, character } = req.body;
 	const response = await llmService.sendMessage(text, params, character);
-	console.log("MSG SENT!")
 
 	//const message = await Message.create({ text, role: 'user', character });
 	//await Message.create({ text: response, role: 'assistant', character });
