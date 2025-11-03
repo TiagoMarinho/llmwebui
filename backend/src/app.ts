10edd48510ca1ddf5c776163b5295ef648772461
Tiago Marinho <tiagomeraki@gmail.com>
2025-11-03 16:31:19 +0100

    remove wrong route path

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,7 +6,6 @@ const app = express();
 
 app.use(express.json());
 app.use("/api/v1/chats", chatRoutes);
-app.use("/api/v1/chats/:chatId/messages", messageRoutes);
 app.use("/api/v1/chats", messageRoutes);
 
 export default app;
