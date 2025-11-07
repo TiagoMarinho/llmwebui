df14299cda404bb584f7e3cd7fbbb525af47e73a
Cauã Oliveira <101019088+Caua-Oliveira@users.noreply.github.com>
2025-11-07 11:26:10 -0300

    changed port to 3000 (5000 wasnt working)

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,7 +5,7 @@ import { connectToDatabase, closeDatabaseConnection } from "./db.ts";
 	try {
 		await connectToDatabase();
 
-		const PORT = process.env.PORT || 5000;
+		const PORT = process.env.PORT || 3000;
 		app.listen(PORT, () => {
 			console.log(`Server running on http://localhost:${PORT}`);
 		});
